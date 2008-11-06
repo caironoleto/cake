334a6ec104a9e458c371452225d07a57635d8b3f
Cairo Noleto <caironoleto@gmail.com>
2008-11-06 09:59:00 -0300

    add messages in generate

--- bin/Generate.php
+++ bin/Generate.php
@@ -6,6 +6,7 @@
 class Generate {
 	private $basepath;
 	function Generate() {
+		print "cake by Cairo Noleto - http://www.caironoleto.com/\n\n";
 	}
 	function setPath($path) {
 		$this->basepath = $path ."/";
@@ -19,70 +20,108 @@ class Generate {
 		if (!file_exists($this->basepath .'system/')) {
 			mkdir($this->basepath .'system/');
 			chmod($this->basepath .'system/', 0775);
+			print "\t\tCreate system/\n";
+		} else {
+			print "\t\tsystem/ exists\n";
 		}
 		if (!file_exists($this->basepath .'system/application/')) {
 			mkdir($this->basepath .'system/application/');
 			chmod($this->basepath .'system/application/', 0775);
+			print "\t\tCreate system/application/\n";
+		} else {
+			print "\t\tsystem/application/ exists\n";
 		}
 		switch($what) {
 			case 'controller':
 				$path = $this->basepath .'system/application/controllers/';
 				if (!file_exists($path)) {
 					mkdir($path);
-					chmod($this->basepath .'system/application/controllers/', 0775);
+					chmod($path, 0775);
+					print "\t\tCreate system/application/controllers/\n";
+				} else {
+					print "\t\tsystem/application/controllers/ exists\n";
 				}
 				$class = ucfirst($name);
 				$file = $class .'Controller.php';
-				$resource = fopen($path .$file, 'w');
-				fwrite($resource, "<?php\n");
-				fwrite($resource, "class $class" ."Controller extends Controller {\n");
-				fwrite($resource, "\tfunction $class" ."Controller() {\n");
-				fwrite($resource, "\t\tparent::Controller();\n");
-				fwrite($resource, "\t}\n");
-				if (is_array($methods)) {
-					foreach ($methods as $method) {
-						fwrite($resource, "\tfunction $method() {\n");
-						fwrite($resource, "\t}\n");
-						$viewpath = $this->basepath .'system/application/views/';
-						if (!file_exists($viewpath)) {
-							mkdir($viewpath);
-							chmod($this->basepath .'system/application/views/', 0775);
+				if (!file_exists($path .$file)) {
+					$resource = fopen($path .$file, 'w');
+					fwrite($resource, "<?php\n");
+					fwrite($resource, "class $class" ."Controller extends Controller {\n");
+					fwrite($resource, "\tfunction $class" ."Controller() {\n");
+					fwrite($resource, "\t\tparent::Controller();\n");
+					fwrite($resource, "\t}\n");
+					print "\t\tCreate system/application/controllers/$file\n";
+					if (is_array($methods)) {
+						foreach ($methods as $method) {
+							fwrite($resource, "\tfunction $method() {\n");
+							fwrite($resource, "\t}\n");
+							print "\t\tAdd $method in " .$class ."Controller\n";
+							$viewpath = $this->basepath .'system/application/views/';
+							if (!file_exists($viewpath)) {
+								mkdir($viewpath);
+								chmod($viewpath, 0775);
+								print "\t\tCreate system/application/views/\n";
+							} else {
+								print "\t\tsystem/application/views/ exists\n";
+							}
+							$viewpath .= $name ."_controller/";
+							if (!file_exists($viewpath)) {
+								mkdir($viewpath);
+								chmod($viewpath, 0775);
+								print "\t\tCreate system/application/views/$name" ."_controller/\n";
+							} else {
+								print "\t\tsystem/application/views/$name" ."_controller/ exists\n" ;
+							}
+							$viewfile = $method ."_view.php";
+							$viewresource = fopen($viewpath .$viewfile, 'w');
+							fclose($viewresource);
+							chmod($viewpath .$viewfile, 0775);
+							print "\t\tCreate system/application/views/$name" ."_controller/$viewfile\n";
 						}
-						$viewpath .= $name ."_controller/";
-						if (!file_exists($viewpath)) {
-							mkdir($viewpath);
-						}
-						$viewfile = $method ."_view.php";
-						$viewresource = fopen($viewpath .$viewfile, 'w');
 					}
+					fwrite($resource, "}\n");
+					fwrite($resource, "?>");
+					fclose($resource);
+					chmod($path .$file, 0775);
+				} else {
+					print "\t\tsystem/application/controllers/$file exists\n";
 				}
-				fwrite($resource, "}\n");
-				fwrite($resource, "?>");
-				fclose($resource);
 			break;
 			case 'model':
 				$path = $this->basepath .'system/application/models/';
-				if (!file_exists($path))
+				if (!file_exists($path)) {
 					mkdir($path);
+					chmod($path, 0775);
+					print "\t\tCreate system/application/models/\n";
+				} else {
+					print "\t\tsystem/application/models/ exists\n";
+				}
 				$class = ucfirst($name);
 				$file = $class .'.php';
-				$resource = fopen($path .$file, 'w');
-				fwrite($resource, "<?php\n");
-				fwrite($resource, "class $class extends Model {\n");
-				fwrite($resource, "\tfunction $class() {\n");
-				fwrite($resource, "\t\tparent::Model();\n");
-				fwrite($resource, "\t}\n");
-				if (is_array($methods)) {
-					foreach ($methods as $method) {
-						fwrite($resource, "\tfunction $method() {\n");
-						fwrite($resource, "\t}\n");
+				if (!file_exists($path .$file)) {
+					$resource = fopen($path .$file, 'w');
+					fwrite($resource, "<?php\n");
+					fwrite($resource, "class $class extends Model {\n");
+					fwrite($resource, "\tfunction $class() {\n");
+					fwrite($resource, "\t\tparent::Model();\n");
+					fwrite($resource, "\t}\n");
+					if (is_array($methods)) {
+						foreach ($methods as $method) {
+							fwrite($resource, "\tfunction $method() {\n");
+							fwrite($resource, "\t}\n");
+						}
 					}
+					fwrite($resource, "}\n");
+					fwrite($resource, "?>");
+					fclose($resource);
+					chmod($path .$file, 0775);
+					print "\t\tCreate system/application/models/$file\n";
+				} else {
+					print "\t\tsystem/application/models/$file exists\n";
 				}
-				fwrite($resource, "}\n");
-				fwrite($resource, "?>");
-				fclose($resource);
 			break;
 		}
+		print "\n";
 	}
 }
 ?>
--- test/BinTest.php
+++ test/BinTest.php
@@ -11,6 +11,10 @@ class BinTest extends PHPUnit_Framework_TestCase {
 		echo exec('php ../bin/cake controller welcome');
 		$this->assertFileExists('system/application/controllers/WelcomeController.php');
 	}
+	function testIfCanCreateViewsToWelcomeController() {
+		echo exec('php ../bin/cake controller user index');
+		$this->assertFileExists('system/application/views/user_controller/index_view.php');
+	}
 	function tearDown() {
 		rm_recursive('system');
 	}
